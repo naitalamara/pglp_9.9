3b342926a042ab4b038bc922abd07f85852a3629
naitalamara <ouali.naitalamara@gmail.com>
2020-05-13 17:32:18 +0200

    creation de la classe Forme

---
+++ exo_9-9/src/main/java/uvsq21807569/exo_9_9/Forme.java
@@ -0,0 +1,21 @@
+package uvsq21807569.exo_9_9;
+
+public abstract class Forme {
+
+	private String nom ;
+
+	public Forme(String nom) {
+		super();
+		this.nom = nom;
+	}
+	
+	public abstract void  afficher();
+	public abstract void deplacer(String nom ,int dx , int dy );
+
+	public String getNom() {
+		return nom;
+	}
+	
+	
+	
+}
