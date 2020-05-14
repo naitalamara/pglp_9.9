b05f6ebacca6b5db899c6806b062e27b7b92d252
naitalamara <ouali.naitalamara@gmail.com>
2020-05-14 16:36:40 +0200

    création des classes cercle,rectangle,carré et triangles

---
+++ exo_9-9/src/main/java/uvsq21807569/exo_9_9/Cercle.java
@@ -0,0 +1,22 @@
+package uvsq21807569.exo_9_9;
+
+public class Cercle extends Forme {
+
+	public Cercle(String nom) {
+		super(nom);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void afficher() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deplacer(String nom, int dx, int dy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
