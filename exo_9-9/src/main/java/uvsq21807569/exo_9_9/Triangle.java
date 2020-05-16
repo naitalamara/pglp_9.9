14fc87b97887dedf7ec9f90d34a79ef71946bb32
naitalamara <ouali.naitalamara@gmail.com>
2020-05-16 13:22:14 +0200

    classe triangle

--- exo_9-9/src/main/java/uvsq21807569/exo_9_9/Triangle.java
+++ exo_9-9/src/main/java/uvsq21807569/exo_9_9/Triangle.java
@@ -2,21 +2,45 @@ package uvsq21807569.exo_9_9;
 
 public class Triangle extends Forme {
 
-	public Triangle(String nom) {
+	 PositonDunPoint a ,b ,c ;
+	 
+	public Triangle(String nom,final  PositonDunPoint p1, final PositonDunPoint p2, final  PositonDunPoint p3) {
 		super(nom);
-		// TODO Auto-generated constructor stub
+		this.a=p1.dupliquer();
+		this.b=p2.dupliquer();
+		this.c=p3.dupliquer();
 	}
 
 	@Override
 	public void afficher() {
-		// TODO Auto-generated method stub
+		System.out.println("Triangle avec le  point a=" + a + ", le point  b=" + b + ",le point  c=" + c + "]");
 		
 	}
 
 	@Override
 	public void deplacer(String nom, int dx, int dy) {
-		// TODO Auto-generated method stub
+		this.a.setX(this.a.getX()+dx);
+		this.b.setX(this.b.getX()+dx);
+		this.c.setX(this.c.getX()+dx);
 		
+		this.a.setY(this.a.getY()+dy);
+		this.b.setY(this.a.getY()+dy);
+		this.c.setY(this.a.getY()+dy);
 	}
 
+	public PositonDunPoint getA() {
+		return a;
+	}
+
+	public PositonDunPoint getB() {
+		return b;
+	}
+
+	public PositonDunPoint getC() {
+		return c;
+	}
+
+	
+
+	
 }
