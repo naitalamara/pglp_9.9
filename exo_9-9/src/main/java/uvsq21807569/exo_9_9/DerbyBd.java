933c1eb2fe2884c8e488825d7e02b2139715fccf
naitalamara <ouali.naitalamara@gmail.com>
2020-05-18 09:22:22 +0200

    creation de la classe Derbybd

---
+++ exo_9-9/src/main/java/uvsq21807569/exo_9_9/DerbyBd.java
@@ -0,0 +1,9 @@
+package uvsq21807569.exo_9_9;
+
+public class DerbyBd {
+	private static final String userName = "";
+	private static final String password = "";
+
+	public static String url = "jdbc:derby:bd;create=true";
+
+}
